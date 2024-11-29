7020e04cbedbe43504d332b3538d4eed9e9d77b0
Natalie Gillam <ngillam@hotmail.co.uk>
2024-11-29 21:25:51 +0000

    queried the DOM for required elements

---
+++ script.js
@@ -0,0 +1,8 @@
+//retrieve the todo from localStorage or initialize an empty array
+
+let todo = JSON.parse(localStorage.getItem("todo")) || [];
+const todoInput = document.getElementById("todoInput");
+const todoList = document.getElementById("todoList");
+const todoCount = document.getElementById("todoCount");
+const addButton = document.querySelector(".btn");
+const deleteButton = document.getElementById("deleteButton");
