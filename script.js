593cc35625556629c88046d1fac018186e28633b
Natalie Gillam <ngillam@hotmail.co.uk>
2024-11-30 14:18:13 +0000

    added event listeners, plus addTask and saveToLocalStorage functions

--- script.js
+++ script.js
@@ -6,3 +6,35 @@ const todoList = document.getElementById("todoList");
 const todoCount = document.getElementById("todoCount");
 const addButton = document.querySelector(".btn");
 const deleteButton = document.getElementById("deleteButton");
+
+document.addEventListener("DOMContentLoaded", () =>{
+addButton.addEventListener("click", () => addTask());
+todoInput.addEventListener("keydown", (e)=>{
+    if(e.key === "Enter"){
+        e.preventDefault();
+
+    }
+})
+deleteButton.addEventListener("click", () => deleteAllTasks());
+displayTasks();
+})
+
+function addTask(){
+    const newTask = todoInput.value.trim();
+    if(newTask !== ""){
+        todo.push({
+            text: newTask, 
+            disabled: false, 
+        });
+        saveToLocalStorage();
+        todoInput.value = "";
+        displayTasks();
+    }
+}
+
+
+
+function saveToLocalStorage(){
+    console.log("Saving to localStorage", todo); // Debugging
+    localStorage.setItem("todo", JSON.stringify(todo));
+}
