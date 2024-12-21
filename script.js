291fd84741dc93e56b6a3a662e11d28479621d6c
Natalie Gillam <ngillam@hotmail.co.uk>
2024-12-21 21:06:20 +0000

    added functionality to switch mode, start timer, pause timer, update timer and reset timer

--- script.js
+++ script.js
@@ -8,6 +8,16 @@ const deleteButton = document.getElementById("deleteButton");
 const toDoH3 = document.querySelector(".to-do-h3");
 const doneH3 = document.querySelector(".done-h3");
 const pomodoroContainer = document.querySelector(".pomodoro-container");
+let time = 25 * 60;
+let timerInterval;
+let minutes;
+let seconds;
+let currentMode = "pomodoro";
+const modes = {
+  pomodoro: 25,
+  short: 5,
+  long: 10,
+};
 
 document.addEventListener("DOMContentLoaded", () => {
   addButton.addEventListener("click", () => addTask());
@@ -62,25 +72,24 @@ function renderTask(currentTask) {
   p.id = currentTask.taskId;
   p.className = currentTask.completed ? "todo-item completed" : "todo-item";
   p.textContent = currentTask.text;
-// ************start*******************
-const startIcon = document.createElement("img");
-startIcon.src = "start.svg";
-//   startIcon.src = "https://assets.codepen.io/6669924/trash.svg";
-startIcon.className = "start-icon";
-startIcon.id = currentTask.taskId;
-startIcon.addEventListener("click", (e) => {
-  startPomodoro(e.target.parentNode.firstChild.children[1].textContent);
-});
-startIcon.addEventListener("keydown", (e) => {
-  if (e.key === "Enter" || e.code === "Space") {
-    e.preventDefault();
+  // ************start*******************
+  const startIcon = document.createElement("img");
+  startIcon.src = "start.svg";
+  //   startIcon.src = "https://assets.codepen.io/6669924/trash.svg";
+  startIcon.className = "start-icon";
+  startIcon.id = currentTask.taskId;
+  startIcon.addEventListener("click", (e) => {
     startPomodoro(e.target.parentNode.firstChild.children[1].textContent);
-  }
-});
-startIcon.setAttribute("tabindex", "0");
+  });
+  startIcon.addEventListener("keydown", (e) => {
+    if (e.key === "Enter" || e.code === "Space") {
+      e.preventDefault();
+      startPomodoro(e.target.parentNode.firstChild.children[1].textContent);
+    }
+  });
+  startIcon.setAttribute("tabindex", "0");
 
-// **************end******************
-
+  // **************end******************
 
   const trashIcon = document.createElement("img");
   trashIcon.src = "trash.svg";
@@ -110,8 +119,8 @@ startIcon.setAttribute("tabindex", "0");
     div.classList.add("completed");
   } else {
     todoList.appendChild(div);
-    div.classList.remove("completed");  
-}
+    div.classList.remove("completed");
+  }
   updateHeaders();
   todoInput.focus();
 }
@@ -129,22 +138,21 @@ function toggleTask(currentTask, element) {
   saveToLocalStorage();
 }
 
-function startPomodoro(id){
+function startPomodoro(id) {
   console.log(id);
   const pomodoroTitle = document.createElement("p");
-    pomodoroTitle.textContent = `Current task: ${id}`;
-    pomodoroTitle.className = "pomodoro-title";
-    pomodoroContainer.textContent="";
-  pomodoroContainer.appendChild(pomodoroTitle); 
+  pomodoroTitle.textContent = `Current task: ${id}`;
+  pomodoroTitle.className = "pomodoro-title";
+  pomodoroContainer.textContent = "";
+  pomodoroContainer.appendChild(pomodoroTitle);
   const timer = document.createElement("p");
   timer.textContent = "25:00";
   const pauseButton = document.createElement("button");
   pauseButton.textContent = "Pause";
   const resetButton = document.createElement("button");
   resetButton.textContent = "Reset";
- const stopButton = document.createElement("button");
+  const stopButton = document.createElement("button");
   stopButton.textContent = "Stop";
-
 }
 
 function deleteTask(id, div) {
@@ -168,4 +176,44 @@ function deleteAllTasks() {
 
 function saveToLocalStorage() {
   localStorage.setItem("todos", JSON.stringify(todos));
+}
+
+document.querySelectorAll("#modes button").forEach((button) => {
+  button.addEventListener("click", handleModeButtons);
+});
+
+function handleModeButtons(e) {
+  console.log(e.target.dataset.modeId);
+  switchMode(e.target.dataset.modeId);
+}
+
+function switchMode(mode) {
+  // Modes are Pomodoro, short break, long break
+  currentMode = mode;
+  resetTimer(currentMode);
+}
+
+function startTimer() {
+  timerInterval = setInterval(updateTimer, 1000);
+}
+
+function pauseTimer() {
+  clearInterval(timerInterval);
+}
+
+function skipTimer() {}
+
+function updateTimer() {
+  time -= 1;
+  minutes = Math.floor(time / 60);
+  seconds = time % 60;
+  minutes = minutes < 10 ? "0" + minutes : minutes;
+  seconds = seconds < 10 ? "0" + seconds : seconds;
+  document.getElementById("timer-label").textContent = minutes + ":" + seconds;
+}
+
+function resetTimer(currentMode) {
+  time = modes[currentMode] * 60;
+  clearInterval(timerInterval);
+  updateTimer();
 }
